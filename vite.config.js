d97204115201f7ad08aeeae695d10831ad845514
Azeez Ololade <ololadeazeez.m@gmail.com>
2025-08-17 21:38:12 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',   // ✅ Vercel expects build output here
+    emptyOutDir: true, // ✅ clears old files before each build
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
+  }
 })
-
-
